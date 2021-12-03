cdadb6a7d9d0412291fa7cca1df3887d77ead44e
gpgun0 <rjsgh7943@gmail.com>
2021-12-04 08:26:37 +0900

    feat: NavBar 로고, 메뉴 배치 & MainPage 백그라운드 이미지

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { ChakraProvider } from '@chakra-ui/react'
 import RootRouter from './route/RootRouter'
 import NavBar from './components/NavBar/NavBar'
 import Footer from './components/Footer/Footer'
+import GlobalStyle from './style/Global'
 
 function App() {
   return (
     <ChakraProvider>
+      <GlobalStyle /> {/* css reset */}
+      <NavBar />
       <div className="App">
-        <NavBar />
         <RootRouter />
         <Footer />
       </div>
