d6090795ce355f87e4e6dcb19dc5c63b0b0784c3
kimjngyun <sanbada79@naver.com>
2021-12-03 18:20:01 +0900

    fix: API 연동 전까지 private route 항상 보이게

--- src/route/PrivateRouter.tsx
+++ src/route/PrivateRouter.tsx
@@ -4,7 +4,8 @@ import { Outlet, Navigate } from 'react-router-dom'
 import { selectUser } from '../app/slices/user/userSlice'
 
 const PrivateRoute = () => {
-  const user = useSelector(selectUser) // Validation logic in here
+  // const user = useSelector(selectUser) // Validation logic in here
+  const user = 'a'
   return user ? <Outlet /> : <Navigate to="/login" />
 }
 
