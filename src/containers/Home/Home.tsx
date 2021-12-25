e47de424381d7de6437d3456538bb105d19a8a7b
leejimin-gist <leejimin0127@gm.gist.ac.kr>
2021-12-25 14:28:32 +0900

    overwritten 해결

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,6 +1,7 @@
 import MainImg from '../../assets/img/gist_summer.jpg'
 import { Search2Icon } from '@chakra-ui/icons'
 import styled from '@emotion/styled'
+import MainPrecaution from './MainPrecaution'
 
 const MainBackgroundImage = styled.div`
   height: 80vh;
@@ -30,12 +31,16 @@ const SearchCurrentPetition__Input = styled.input`
 `
 
 // 토글 기능 추가해야합니다.
-const Precautions = styled.div`
+const Precaution = styled.div`
   background-color: #c4c4c4; // 잠정적인 컬러
-  height: 60px;
+  height: 80px;
+  position: relative;
 `
 
 const DashBoard = styled.header`
+  position:relative;
+  padding: 10px;
+  height 200px;
   text-align: center;
   line-height: 1.4;
   font-size: 32px;
@@ -60,12 +65,16 @@ const Home = (): JSX.Element => {
             </SearchCurrentPetition>
           </div>
         </MainBackgroundImage>
-        <Precautions></Precautions>
       </div>
+      <Precaution>
+        <MainPrecaution />
+      </Precaution>
       <DashBoard>
-        지금까지,
-        <br />총 <span style={{ color: '#D52425' }}>00</span>건의 청원과{' '}
-        <span style={{ color: '#D52425' }}>00</span>건의 답변이 올라왔습니다.
+        <div>
+          지금까지,
+          <br />총 <span style={{ color: '#D52425' }}>00</span>건의 청원과{' '}
+          <span style={{ color: '#D52425' }}>00</span>건의 답변이 올라왔습니다.
+        </div>
       </DashBoard>
     </>
   )
