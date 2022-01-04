4d34436ec35b98c795d0e231ec6b6d5480b23bc5
kimjngyun <sanbada79@naver.com>
2022-01-04 15:26:15 +0900

    feat: rkt로 login api 연결시도

--- src/route/PrivateRouter.tsx
+++ src/route/PrivateRouter.tsx
@@ -1,11 +1,11 @@
 // import React from 'react'
-import { useSelector } from 'react-redux'
 import { Outlet, Navigate } from 'react-router-dom'
-import { selectUser } from '../app/slices/user/userSlice'
+import { getUserInfo } from '../app/slices/user/userSlice'
+import { useAppSelect } from '../app/store.hooks'
 
 const PrivateRoute = () => {
-  // const user = useSelector(selectUser) // Validation logic in here
-  const user = 'a'
+  const user = useAppSelect(getUserInfo) // Validation logic in here
+  // const user = 'a'
   return user ? <Outlet /> : <Navigate to="/login" />
 }
 
