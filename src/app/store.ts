4522dd4a181297ca282302feef5933b9f68d38b6
kimjngyun <sanbada79@naver.com>
2022-01-08 23:35:55 +0900

    로그인 방식 변경으로 인해 코드 수정 시작

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
-import rootReducer from './slices'
+import rootReducer from './rootReducer'
 
 const store = configureStore({
   reducer: rootReducer,
 })
+
+/* HMR
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./rootReducer', () => {
+    const newRootReducer = require('./rootReducer').preventDefault()
+    store.replaceReducer(newRootReducer)
+  })
+}
+*/
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -1,6 +1,6 @@
 import React, { FormEvent, useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { addUserAsync, User } from '../../app/slices/user/userSlice'
+import { loginUserAsync, User } from '../../app/userSlice'
 import {
   chakra,
   FormControl,
@@ -10,6 +10,7 @@ import {
   Stack,
   Text,
 } from '@chakra-ui/react'
+import { stackStyle } from './style'
 import { FaUserAlt, FaLock } from 'react-icons/fa'
 import styled from '@emotion/styled'
 
@@ -24,65 +25,53 @@ const Login = (): JSX.Element => {
   // chakra icon
   const CFaUserAlt = chakra(FaUserAlt)
   const CFaLock = chakra(FaLock)
-  const [user, setUser] = useState<User>({ email: '', password: '' })
+  const [user, setUser] = useState<User>({ username: '', password: '' })
 
   const handleChangeUser = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // console.log(e.target)
-    const { type, value } = e.target
-    setUser({ ...user, [type]: value })
+    console.log(e.target.name)
+    const { name, value } = e.target
+    setUser({ ...user, [name]: value })
   }
   const dispatch = useDispatch()
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    dispatch(addUserAsync(user))
+    dispatch(loginUserAsync(user))
   }
   return (
     <div className="login">
-      <form className="login__form" onSubmit={e => handleSubmit(e)}>
-        <Stack
-          spacing={4}
-          style={{
-            position: 'absolute',
-            top: '300px',
-            left: '0',
-            right: '0',
-            height: '360px',
-            width: '400px',
-            margin: 'auto',
-          }}
-        >
+      <form className="login__form" onSubmit={handleSubmit}>
+        <Stack spacing={4} style={stackStyle}>
           <Text fontSize="4xl" fontWeight="bold">
             로그인
           </Text>
-
           <FormControl isRequired>
             <Text mb="8px">이메일</Text>
             <InputGroup borderColor="#ccc">
               <InputLeftElement pointerEvents="none">
                 {<CFaUserAlt color="gray.300" />}
               </InputLeftElement>
-
               <Input
                 type="email"
+                name="username"
                 placeholder="이메일을 입력하세요."
-                value={user.email}
-                onChange={e => handleChangeUser(e)}
+                value={user.username}
+                onChange={handleChangeUser}
               />
             </InputGroup>
           </FormControl>
-
+          <p></p>
           <FormControl>
             <Text mb="8px">비밀번호</Text>
-
             <InputGroup borderColor="#ccc">
               <InputLeftElement pointerEvents="none">
                 {<CFaLock color="gray.300" />}
               </InputLeftElement>
               <Input
                 type="password"
+                name="password"
                 placeholder="비밀번호를 입력하세요."
                 value={user.password}
-                onChange={e => handleChangeUser(e)}
+                onChange={handleChangeUser}
               />
             </InputGroup>
           </FormControl>
