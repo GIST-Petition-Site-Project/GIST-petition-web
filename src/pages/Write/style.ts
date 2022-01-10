efaab1e5faf67573861893e9a222d375fcbc99e9
kimjngyun <sanbada79@naver.com>
2022-01-10 20:09:57 +0900

    refact: Write style 코드 분리

---
+++ src/pages/Write/style.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+const Inner = styled.div`
+  position: relative;
+  margin: 0 auto;
+  max-width: 900px;
+  height: 100%;
+`
+
+const StyledBoxInner = styled.div`
+  margin: 30px;
+`
+
+export { Inner, StyledBoxInner }
