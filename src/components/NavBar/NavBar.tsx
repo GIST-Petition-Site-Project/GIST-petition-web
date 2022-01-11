b21e89290756fa99a44484a32f730b7e3e325b65
kimjngyun <sanbada79@naver.com>
2022-01-11 21:42:41 +0900

    refact: NavBar에서 style 코드 분리

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,54 +1,10 @@
 import logo from '../../assets/img/logo_light.png'
-import styled from '@emotion/styled'
+import { Header, Inner, Logo, Logo__Image, Menu, ItemName } from './style'
 import { getUsersMe } from '../../utils/api/getUsersMe'
 import { setLogin, setLogout } from '../../redux/auth/authSlice'
 import { useDispatch } from 'react-redux'
 import { useAppSelect } from '../../redux/store.hooks'
 import { postLogout } from '../../utils/api/postLogout'
-
-const Header = styled.header`
-  height: 60px;
-  width: 100%;
-  position: fixed; /* 모바일 환경에선 sticky를 없애거나 헤더를 줄이기 */ /* fixed -> display: block 자동 적용 */
-  top: 0;
-  z-index: 1000;
-  background-color: rgba(47, 54, 60, 0.9);
-  //#384046, rgba로 opacity를 먹여야 children 요소가 투명해지지 않습니다.
-`
-const Inner = styled.div`
-  position: relative;
-  margin: 0 auto;
-  max-width: 900px;
-  height: 100%;
-`
-
-const Logo = styled.div``
-
-const Logo__Image = styled.img`
-  width: 128px;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  margin: auto 0;
-`
-const Menu = styled.ul`
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  display: flex;
-`
-const ItemName = styled.div`
-  margin: 0px 0px 5px 40px;
-  padding: 5px 0px 3px 0px;
-  font-size: 18px;
-  font-weight: bold;
-  color: white;
-  border: 2px solid transparent;
-  &:hover {
-    cursor: pointer;
-    border-bottom: 2px solid #d52425;
-  }
-`
 
 const NavBar = (): JSX.Element => {
   const dispatch = useDispatch()
