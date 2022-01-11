b462b7885ade098f9acc8c2684098ee25e28d98a
kimjngyun <sanbada79@naver.com>
2022-01-11 21:04:06 +0900

    feat: GET users/me 요청코드 작성

--- src/utils/api/getUsersMe.ts
+++ src/utils/api/getUsersMe.ts
@@ -3,7 +3,6 @@ import { config } from './axiosConfigs'
 const URL: string = process.env.REACT_APP_BASE_URL as string
 
 export const postLogin = async (user: User) => {
-  console.log(user)
-  const response = await axios.post(`${URL}login`, user, config)
+  const response = await axios.get(`${URL}users/me`, config)
   return response.status
 }
