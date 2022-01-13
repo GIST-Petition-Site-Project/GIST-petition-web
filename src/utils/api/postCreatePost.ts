ce52c88f20b1689fec7f01f4d899b764d8ee36a5
kimjngyun <sanbada79@naver.com>
2022-01-13 16:01:31 +0900

    fix: getCreatePost => postCreatePost typo 수정

--- src/utils/api/postCreatePost.ts
+++ src/utils/api/postCreatePost.ts
@@ -1,6 +1,6 @@
 import api from './axiosConfigs'
 
-export const getCreatePost = async (payload: PostsInput) => {
+export const postCreatePost = async (payload: PostsInput) => {
   const response = await api.post('posts', payload)
   return response.status
 }
