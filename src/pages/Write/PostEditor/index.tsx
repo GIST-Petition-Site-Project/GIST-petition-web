48d48c8b11e92b0d2e9276126fc110f0ab9e1029
kimjngyun <sanbada79@naver.com>
2022-01-13 15:52:20 +0900

    chore: api import 경로 수정

--- src/pages/Write/PostEditor/index.tsx
+++ src/pages/Write/PostEditor/index.tsx
@@ -11,7 +11,7 @@ import {
   Textarea,
 } from '@chakra-ui/react'
 import { SubmitButton, BackButton } from './styles'
-import { getCreatePost } from '../../../utils/api/getCreatePost'
+import { getCreatePost } from '../../../utils/api'
 import { useNavigate } from 'react-router-dom'
 
 const PostEditor = () => {
