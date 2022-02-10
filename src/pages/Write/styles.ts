1d9a9e34e8a28f83397a4301758141f933de5cf3
gpgun0 <rjsgh7943@gmail.com>
2022-02-10 14:53:12 +0900

    desing: 청원 작성 페이지 반응형

--- src/pages/Write/styles.ts
+++ src/pages/Write/styles.ts
@@ -1,30 +1,26 @@
 import styled from '@emotion/styled'
+import theme from '../../style/theme'
 import { Box, Stack } from '@chakra-ui/react'
+
 const Inner = styled.div`
   position: relative;
+  max-width: ${theme.space.INNER_MAXWIDTH};
   margin: 0 auto;
-  max-width: 900px;
   height: 100%;
+  padding: 0 2rem;
 `
 
 const WriteStack = styled(Stack)`
-  position: absolute;
-  top: 150px;
-  bottom: 20px;
-  width: 100%;
+  position: relative;
+  top: 9.375rem;
 `
 const Wrapper = styled(Box)`
   border: 1px solid #ccc;
   border-radius: 0;
-  padding: 10px;
   letter-spacing: wide;
 `
 
-const StyledBoxInner = styled.div`
-  margin: 30px;
-`
-const StyledPostEditor = styled.div`
-  margin: 60px 30px;
-`
+const StyledBoxInner = styled(Box)``
+const StyledPostEditor = styled(Box)``
 
 export { Inner, Wrapper, WriteStack, StyledBoxInner, StyledPostEditor }
