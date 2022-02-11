198f70be547753d65233532650f560387c8d4eb1
leejimin-gist <leejimin0127@gm.gist.ac.kr>
2022-02-12 01:28:53 +0900

    refactor: pagination 버튼 컴포넌트 분리

--- src/pages/MyPetitions/index.tsx
+++ src/pages/MyPetitions/index.tsx
@@ -1,12 +1,116 @@
 import PetitionList from '../../components/PetitionList'
 import { getMyPetitionsByQuery } from '../../utils/api'
+import { Inner, PetitionBoard } from './styles'
+import { Stack } from '@chakra-ui/react'
+import qs from 'qs'
+import { ChangeEvent, useState } from 'react'
+import { PetitionsSelect, PetitionsText, PetitionsTitle } from './styles'
+import { Category } from '../../types/enums'
+import { useNavigate } from 'react-router-dom'
+import { PetitionTitle } from '../Petition/PetitionContents/styles'
+import PaginationButtons from '../../components/PaginationButtons'
 
 const MyPetitions = (): JSX.Element => {
+  const queryParams: any = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  })
+
+  const numberOfCategory = Object.keys(Category).filter(el =>
+    isNaN(Number(el)),
+  ).length
+
+  const catergoryIdx = Array(numberOfCategory)
+    .fill(0)
+    .map((_x, i) => i)
+
+  const [selected, setSelected] = useState(queryParams?.category || 0)
+  const navigate = useNavigate()
+  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelected(Number(e.target.value))
+    const newSearchParams = {
+      ...queryParams,
+      page: 1,
+      category: Number(e.target.value),
+    }
+    navigate({
+      pathname: '/mypetitions',
+      search: new URLSearchParams(newSearchParams).toString(),
+    })
+  }
   return (
     <>
-      <PetitionList getPetitions={getMyPetitionsByQuery} />
+      <Inner>
+        <PetitionBoard>
+          <PetitionsTitle>
+            <PetitionsText>나의 청원</PetitionsText>
+          </PetitionsTitle>
+          <PetitionList getPetitions={getMyPetitionsByQuery} />
+          <Stack>
+            <PaginationButtons
+              getPetitions={getMyPetitionsByQuery}
+              pathname={'/mypetitions'}
+            />
+          </Stack>
+        </PetitionBoard>
+      </Inner>
     </>
   )
 }
 
 export default MyPetitions
+
+{
+  /*
+import { getPetitionsByQuery } from '../../utils/api'
+
+const Petitions = (): JSX.Element => {
+  const queryParams: any = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  })
+
+  const numberOfCategory = Object.keys(Category).filter(el =>
+    isNaN(Number(el)),
+  ).length
+
+  const catergoryIdx = Array(numberOfCategory)
+    .fill(0)
+    .map((_x, i) => i)
+
+  const [selected, setSelected] = useState(queryParams?.category || 0)
+  const navigate = useNavigate()
+  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelected(Number(e.target.value))
+    const newSearchParams = {
+      ...queryParams,
+      page: 1,
+      category: Number(e.target.value),
+    }
+    navigate({
+      pathname: '/petitions',
+      search: new URLSearchParams(newSearchParams).toString(),
+    })
+  }
+  return (
+    <Inner>
+      <PetitionBoard>
+        <PetitionsTitle>
+          <PetitionsText>나의 청원</PetitionsText>
+          <PetitionsSelect onChange={handleSelect} value={selected} w={'128px'}>
+            {catergoryIdx.map(item => (
+              <option value={item} key={item}>
+                {Category[item]}
+              </option>
+            ))}
+          </PetitionsSelect>
+        </PetitionsTitle>
+        <PetitionList getPetitions={getPetitionsByQuery}></PetitionList>
+        <Stack>
+          <PaginationButtons></PaginationButtons>
+        </Stack>
+      </PetitionBoard>
+    </Inner>
+  )
+}
+
+*/
+}
