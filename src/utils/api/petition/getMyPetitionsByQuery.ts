4c19a32fcd0d7d9951d472e973088f349edcf6b0
gpgun0 <rjsgh7943@gmail.com>
2022-02-12 17:37:16 +0900

    chore: 페이지네이션 버튼 누를때마다 콘솔 메세지 찍히는 것 삭제

--- src/utils/api/petition/getMyPetitionsByQuery.ts
+++ src/utils/api/petition/getMyPetitionsByQuery.ts
@@ -4,7 +4,6 @@ export const getMyPetitionsByQuery = async (query: QueryParams) => {
   const size = Number(query?.size) || 10
   const page = Number(query?.page) || 1
   const category = Number(query?.category) || 0
-  console.log(size, page, category)
   const response = await api.get(
     `petitions/me?size=${size}&page=${page - 1}&categoryId=${category}`,
   )
