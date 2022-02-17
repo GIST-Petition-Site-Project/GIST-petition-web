249fb1912b336c20276bda2b9d67d6225d778b84
leejimin-gist <leejimin0127@gm.gist.ac.kr>
2022-02-17 11:26:50 +0900

    refactor: 나의 청원, 답변된 청원 필요없는 코드 삭제

--- src/pages/AnsweredPetitions/index.tsx
+++ src/pages/AnsweredPetitions/index.tsx
@@ -1,42 +1,14 @@
 import { Inner, PetitionBoard } from './styles'
 import { Stack } from '@chakra-ui/react'
-import qs from 'qs'
-import { ChangeEvent, useState } from 'react'
 import { PetitionsText, PetitionsTitle } from './styles'
-import { Category } from '../../types/enums'
 import { useNavigate } from 'react-router-dom'
 import PaginationButtons from '../../components/PaginationButtons'
-import { getMyPetitionsByQuery } from '../../utils/api'
 import PetitionList from '../../components/PetitionList'
 import { getAnweredPetitionsByQuery } from '../../utils/api/petition/getAnsweredPetitionsByQuery'
 
 const AnsweredPetitions = (): JSX.Element => {
-  const queryParams: any = qs.parse(location.search, {
-    ignoreQueryPrefix: true,
-  })
-
-  const numberOfCategory = Object.keys(Category).filter(el =>
-    isNaN(Number(el)),
-  ).length
-
-  const catergoryIdx = Array(numberOfCategory)
-    .fill(0)
-    .map((_x, i) => i)
-
-  const [selected, setSelected] = useState(queryParams?.category || 0)
   const navigate = useNavigate()
-  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
-    setSelected(Number(e.target.value))
-    const newSearchParams = {
-      ...queryParams,
-      page: 1,
-      category: Number(e.target.value),
-    }
-    navigate({
-      pathname: '/mypetitions',
-      search: new URLSearchParams(newSearchParams).toString(),
-    })
-  }
+
   return (
     <>
       <Inner>
--- src/pages/MyPetitions/index.tsx
+++ src/pages/MyPetitions/index.tsx
@@ -1,41 +1,13 @@
 import { Inner, PetitionBoard } from './styles'
 import { Stack } from '@chakra-ui/react'
-import qs from 'qs'
-import { ChangeEvent, useState } from 'react'
 import { PetitionsText, PetitionsTitle } from './styles'
-import { Category } from '../../types/enums'
 import { useNavigate } from 'react-router-dom'
 import PaginationButtons from '../../components/PaginationButtons'
 import { getMyPetitionsByQuery } from '../../utils/api'
 import MyPetitionList from './MyPetitionList'
 
 const MyPetitions = (): JSX.Element => {
-  const queryParams: any = qs.parse(location.search, {
-    ignoreQueryPrefix: true,
-  })
-
-  const numberOfCategory = Object.keys(Category).filter(el =>
-    isNaN(Number(el)),
-  ).length
-
-  const catergoryIdx = Array(numberOfCategory)
-    .fill(0)
-    .map((_x, i) => i)
-
-  const [selected, setSelected] = useState(queryParams?.category || 0)
   const navigate = useNavigate()
-  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
-    setSelected(Number(e.target.value))
-    const newSearchParams = {
-      ...queryParams,
-      page: 1,
-      category: Number(e.target.value),
-    }
-    navigate({
-      pathname: '/mypetitions',
-      search: new URLSearchParams(newSearchParams).toString(),
-    })
-  }
   return (
     <>
       <Inner>
