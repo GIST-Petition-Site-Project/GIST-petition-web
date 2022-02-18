96da59850d6a2f356b5d21f58f9d1a66fbd56208
kimjngyun <sanbada79@naver.com>
2022-02-19 00:15:32 +0900

    fix: 요청을 보내지 않는 경우 empty response 설정

--- src/utils/api/petition/getAgreements.ts
+++ src/utils/api/petition/getAgreements.ts
@@ -2,7 +2,10 @@ import api from '../axiosConfigs'
 
 export const getAgreements = async (petitionId: string) => {
   if (petitionId.length === 6 || petitionId === 'undefined')
-    return { status: 500 }
+    return {
+      status: 500,
+      data: null,
+    }
   const response = await api.get(`petitions/${petitionId}/agreements`)
   return response
 }
