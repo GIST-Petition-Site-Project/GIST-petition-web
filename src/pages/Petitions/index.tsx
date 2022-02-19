ef3df0a3331b38d976a471f819b54583ba730671
leejimin-gist <leejimin0127@gm.gist.ac.kr>
2022-02-19 21:12:03 +0900

    feat: 모든청원 tab 구현

--- src/pages/Petitions/index.tsx
+++ src/pages/Petitions/index.tsx
@@ -6,10 +6,19 @@ import PetitionList from '../../components/PetitionList'
 import PaginationButtons from '../../components/PaginationButtons'
 import qs from 'qs'
 import { ChangeEvent, useState } from 'react'
-import { PetitionsSelect, PetitionsText, PetitionsTitle } from './styles'
+import {
+  PetitionsSelect,
+  PetitionsText,
+  PetitionsTitle,
+  SelectWrapper,
+  PetitionsTab,
+  PetitionsTabs,
+  PetitionsTabList,
+} from './styles'
 import { Category } from '../../types/enums'
 import { useNavigate } from 'react-router-dom'
 import { getPetitionsByQuery } from '../../utils/api'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
 const Petitions = (): JSX.Element => {
   const queryParams: any = qs.parse(location.search, {
@@ -43,15 +52,52 @@ const Petitions = (): JSX.Element => {
       <PetitionBoard>
         <PetitionsTitle>
           <PetitionsText>모든 청원</PetitionsText>
-          <PetitionsSelect onChange={handleSelect} value={selected} w={'128px'}>
-            {catergoryIdx.map(item => (
-              <option value={item} key={item}>
-                {Category[item]}
-              </option>
-            ))}
-          </PetitionsSelect>
         </PetitionsTitle>
-        <PetitionList getPetitions={getPetitionsByQuery}></PetitionList>
+        <PetitionsTabs isFitted>
+          <PetitionsTabList>
+            <PetitionsTab>진행중인 청원</PetitionsTab>
+            <PetitionsTab>추천순 청원</PetitionsTab>
+            <PetitionsTab borderRight="2px solid #333">
+              만료된 청원
+            </PetitionsTab>
+          </PetitionsTabList>
+          <TabPanels>
+            <TabPanel>
+              <SelectWrapper>
+                <PetitionsText>진행중인 청원</PetitionsText>
+                <PetitionsSelect
+                  onChange={handleSelect}
+                  value={selected}
+                  w={'128px'}
+                >
+                  {catergoryIdx.map(item => (
+                    <option value={item} key={item}>
+                      {Category[item]}
+                    </option>
+                  ))}
+                </PetitionsSelect>
+              </SelectWrapper>
+              <PetitionList getPetitions={getPetitionsByQuery}></PetitionList>
+            </TabPanel>
+            <TabPanel>
+              <SelectWrapper>
+                <PetitionsText h="40px" mb="1px">
+                  추천순 청원
+                </PetitionsText>
+              </SelectWrapper>
+              <PetitionList getPetitions={getPetitionsByQuery}></PetitionList>
+            </TabPanel>
+            <TabPanel>
+              <SelectWrapper>
+                <PetitionsText h="40px" mb="1px">
+                  만료된 청원
+                </PetitionsText>
+              </SelectWrapper>
+              <PetitionList getPetitions={getPetitionsByQuery}></PetitionList>
+            </TabPanel>
+          </TabPanels>
+        </PetitionsTabs>
+
         <Stack>
           <PaginationButtons
             getPetitions={getPetitionsByQuery}
--- src/pages/Petitions/styles.ts
+++ src/pages/Petitions/styles.ts
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled'
 import theme from '../../style/theme'
-import { Box, Select, Text } from '@chakra-ui/react'
+import { Box, Select, Text, Tab, Tabs, TabList } from '@chakra-ui/react'
 const Inner = styled.div`
   position: relative;
   margin: 0 auto;
@@ -18,15 +18,16 @@ const PetitionBoard = styled.div`
 const PetitionsTitle = styled(Box)`
   display: flex;
   justify-content: space-between;
-  padding-bottom: 5px;
-  border-bottom: 2px solid #333;
+  padding-bottom: 30px;
 `
 
 const PetitionsSelect = styled(Select)`
-  width: 128px;
+  width: 100%;
   height: 40px;
   border-radius: 0;
   border-color: #ccc;
+  top: 0;
+  right: 0;
 `
 
 const PetitionsText = styled(Text)`
@@ -34,4 +35,34 @@ const PetitionsText = styled(Text)`
   font-weight: bold;
 `
 
-export { Inner, PetitionBoard, PetitionsTitle, PetitionsSelect, PetitionsText }
+const SelectWrapper = styled.div`
+  border-bottom: 2px solid #333;
+  padding-bottom: 10px;
+  display: flex;
+  justify-content: space-between;
+`
+const PetitionsTab = styled(Tab)`
+  border-top: 2px solid #333;
+  border-left: 2px solid #333;
+  font-weight: bold;
+  :focus {
+    outline: none;
+    box-shadow: none;
+  }
+`
+const PetitionsTabs = styled(Tabs)``
+const PetitionsTabList = styled(TabList)`
+  border-bottom: 0px;
+`
+
+export {
+  Inner,
+  PetitionBoard,
+  PetitionsTitle,
+  PetitionsSelect,
+  PetitionsText,
+  SelectWrapper,
+  PetitionsTab,
+  PetitionsTabs,
+  PetitionsTabList,
+}
