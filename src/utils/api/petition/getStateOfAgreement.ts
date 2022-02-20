8f15bee66b4653fcb7dcab00cc09a91aace44f1c
Kim-GeonHo <rjsgh7943@gmail.com>
2022-02-20 22:47:38 +0900

    fix: 나의 청원 페이지 - tempUrl로 게시글들 보여주기 (#198)
    * fix: 나의 청원 페이지 - tempUrl로 게시글들 보여주기
    
    * fix: 동의 안했는데  동의하기 버튼 비활성화인 현상 해결

--- src/utils/api/petition/getStateOfAgreement.ts
+++ src/utils/api/petition/getStateOfAgreement.ts
@@ -2,7 +2,7 @@ import api from '../axiosConfigs'
 
 export const getStateOfAgreement = async (petitionId: string) => {
   if (petitionId.length === 6 || petitionId === 'undefined')
-    return { status: 500 }
+    return { status: 500, data: true }
   const response = await api.get(`petitions/${petitionId}/agreements/me`)
   return response
 }
