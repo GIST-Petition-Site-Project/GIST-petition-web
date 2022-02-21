f167185e9bcc3280225ac93396509f67319dd367
gpgun0 <rjsgh7943@gmail.com>
2022-02-22 02:33:11 +0900

    design: 청원하기 페이지 패딩값 조정

--- src/pages/Write/styles.ts
+++ src/pages/Write/styles.ts
@@ -1,19 +1,41 @@
 import styled from '@emotion/styled'
-// import theme from '../../style/theme'
-import { Box } from '@chakra-ui/react'
+import theme from '../../style/theme'
 
 const Container = styled.div`
   .inner {
     padding: 0 0.5rem;
+    @media screen and (min-width: ${theme.breakpoints.md}) {
+      padding: 0 2rem;
+    }
     > .chakra-stack {
       position: relative;
       margin: 9.375rem 0;
+      h2 {
+        font-size: 20px;
+      }
     }
   }
 `
-const WriteWrapper = styled(Box)`
+
+const WriteContainer = styled.div`
   border: 1px solid #ccc;
   border-radius: 0;
+  .write_wrapper {
+    margin: 1rem;
+    @media screen and (min-width: ${theme.breakpoints.md}) {
+      margin: 2rem;
+    }
+    .principle_section {
+      margin: 50px 0;
+    }
+
+    .editor_section {
+      margin: 60px 0;
+      @media screen and (min-width: ${theme.breakpoints.sm}) {
+        margin: 60px 18px;
+      }
+    }
+  }
 `
 
-export { Container, WriteWrapper }
+export { Container, WriteContainer }
