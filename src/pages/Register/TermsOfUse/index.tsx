dd9c7e53c6f8f9c12f7ff3da7ebfe12c47d31607
gpgun0 <rjsgh7943@gmail.com>
2022-02-24 16:45:02 +0900

    fix: build failed: prettier 적용

--- src/pages/Register/TermsOfUse/index.tsx
+++ src/pages/Register/TermsOfUse/index.tsx
@@ -1,4 +1,3 @@
-
 import { memo, useCallback, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import TermsOfUseAccordion from '../../../components/Register/Accordion'
@@ -10,7 +9,6 @@ import { TermsOfUseBox } from './styles'
 const TermsOfUse = memo((): JSX.Element => {
   const agreeInfo = useSelector((state: RootState) => state.register.agreeInfo)
   const dispatch = useDispatch()
-
 
   const handleAgree = (value: string) => {
     switch (value) {
