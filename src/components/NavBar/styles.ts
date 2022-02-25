fb77c8a54ba310da857447bee7939e1a111a6fa8
gpgun0 <rjsgh7943@gmail.com>
2022-02-25 14:29:03 +0900

    style: NavBar/index.tsx 스타일 코드 따로 빼놓기

--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -92,6 +92,18 @@ const MobMenuButton = styled(Button)`
   right: 0;
   height: 100%;
   transform: ${props => (props.open ? 'rotate(-90deg)' : 'none')};
+  display: block;
+  /* background: rgba(47, 54, 60, 0.9); */
+  background: transparent;
+  @media screen and (min-width: ${theme.breakpoints.md}) {
+    display: none;
+  }
+  :focus {
+    box-shadow: none;
+  }
+  :hover {
+    background: transparent;
+  }
 `
 
 export { Header, Logo, TopMenu, ItemName, MobMenuButton }
