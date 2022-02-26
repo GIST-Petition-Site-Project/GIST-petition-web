bfb844f0b11ecdee8c041ad5cb00486e96b9232e
Kim-GeonHo <rjsgh7943@gmail.com>
2022-02-26 15:10:20 +0900

    fix: non-boolean attribute 워닝 해결

--- src/pages/MyPetitions/MyPetitionList/index.tsx
+++ src/pages/MyPetitions/MyPetitionList/index.tsx
@@ -46,7 +46,7 @@ const MyPetitionList = ({ getPetitions }: GetPetitions): JSX.Element => {
           <li key={petition.id}>
             <PetitionTagWrapper>
               <PetitionStatus>
-                <PetitionStatusTag expired={petition.expired}>
+                <PetitionStatusTag expired={String(petition.expired)}>
                   {petition.expired ? '사전동의만료' : '사전동의중'}
                 </PetitionStatusTag>
               </PetitionStatus>
