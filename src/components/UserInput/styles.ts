02fe3e69bb3431db6e4c17a71c938cfc0f79d250
gpgun0 <rjsgh7943@gmail.com>
2022-02-28 11:56:24 +0900

    chore: input placeholder 메세지 변경

--- src/components/UserInput/styles.ts
+++ src/components/UserInput/styles.ts
@@ -1,9 +1,18 @@
-import { Text } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 
-const RegisterText = styled(Text)`
-  margin-top: 1em;
-  margin-bottom: 0.5em;
+const SUserInput = styled.div`
+  .page_type {
+    margin-top: 1em;
+    margin-bottom: 0.5em;
+  }
+
+  input {
+  }
+  .chakra-button {
+    :focus {
+      box-shadow: none;
+    }
+  }
 `
 
-export { RegisterText }
+export { SUserInput }
