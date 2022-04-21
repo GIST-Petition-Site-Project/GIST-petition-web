f299fde5cd0d60c2135d695e62a1b8fb5562285f
Kim-GeonHo <rjsgh7943@gmail.com>
2022-04-22 05:37:49 +0900

    fix: 청원 글 쓰기 띄어쓰기 두번 연속 제한 해제 (#330)

--- src/pages/Write/PostEditor/index.tsx
+++ src/pages/Write/PostEditor/index.tsx
@@ -52,7 +52,7 @@ const PostEditor = () => {
       }
       setPetitionInput({
         ...petitionInput,
-        [name]: value.replace(/ +/g, ' '),
+        [name]: value,
       })
     },
     [petitionInput],
