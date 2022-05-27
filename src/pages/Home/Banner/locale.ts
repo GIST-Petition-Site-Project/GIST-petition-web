be1d287aec36415048899d640ef566cdc7cc4120
gpgun0 <rjsgh7943@gmail.com>
2022-05-27 09:44:00 +0900

    banner 영문 번역

---
+++ src/pages/Home/Banner/locale.ts
@@ -0,0 +1,14 @@
+const locale: Locale = {
+  ko: {
+    above1: '지금까지 총 ',
+    above2: ' 개의 청원과 ',
+    below: ' 개의 답변이 등록되었습니다',
+  },
+  en: {
+    above1: 'Total ',
+    above2: ' petitions are requested, ',
+    below: ' are answered',
+  },
+}
+
+export default locale
